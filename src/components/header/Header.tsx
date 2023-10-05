36e4318a3cccd10493260e93c57c69a4cd01c3c2
SStandby <standbyy9@gmail.com>
2023-10-05 11:40:02 -0500

    refactor: padding settings

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -28,11 +28,11 @@ export default component$(() => {
     },
   ];
   return (
-    <header class="fixed z-20 flex w-[100vw] items-center p-6  lg:px-8 justify-between">
+    <header class="fixed z-20 flex w-[100vw] items-center p-8 lg:px-20 justify-between">
       <div class="w-14 h-14  p-1  rounded-full bg-gradient-to-r from-rose-400 via-pink-400 to-purple-400">
         <Image class="rounded-full" />
       </div>
-      <nav class="flex p-6 px-10  rounded-full border  border-zinc-400 text-zinc-300 gap-4 ">
+      <nav class="flex p-4 px-10  rounded-full border  border-zinc-400 text-zinc-300 gap-4 ">
         {icons.map(({ icon, name }, index) => (
           <button
             key={index}

51dcf532a3ffe296743c7c40d87b03070e01d5ab
SStandby <standbyy9@gmail.com>
2023-10-05 23:37:23 -0500

    refactor: gradient effect slider

--- src/components/sliderIcons/SliderIcons.tsx
+++ src/components/sliderIcons/SliderIcons.tsx
@@ -34,7 +34,7 @@ export default component$(() => {
     },
   ];
   return (
-    <div class="relative m-auto w-[924px] overflow-hidden">
+    <div class="relative rounded-full m-auto w-[1024px] overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,rgba(16,18,23)_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,rgba(16,18,23)_0%,rgba(255,255,255,0)_100%)] after:content-['']">
       <div class="animate-infinite-slider flex w-[calc(250px*10)] text-6xl text-zinc-200">
         {icons.map(({ icon, link }, index) => (
           <Link key={index} href={link}>

f205c3e310901a1134fca4a6e5311b72e7a5f141
SStandby <standbyy9@gmail.com>
2023-10-07 00:27:54 -0500

    feat: effect shine box

---
+++ src/components/tagShine/TagShine.tsx
@@ -0,0 +1,13 @@
+import { component$, Slot } from "@builder.io/qwik";
+
+export interface TagShineProps {
+  text: string;
+}
+
+export default component$(({ text }: TextGradientProps) => {
+  return (
+    <span className="inline-flex h-full w-20 animate-background-shine cursor-pointer items-center justify-center rounded-full border border-slate-400 bg-[linear-gradient(110deg,#000,45%,#4D4B4B,55%,#000)]  bg-[length:250%_100%] px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl custom-text-shadow">
+      {text}
+    </span>
+  );
+});

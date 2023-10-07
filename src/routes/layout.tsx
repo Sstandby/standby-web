20c0751ab9d3363e946dfa72d31951b6787774b6
SStandby <standbyy9@gmail.com>
2023-10-06 23:10:11 -0500

    refactor: heigth min screen

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -15,7 +15,7 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 
 export default component$(() => {
   return (
-    <div class="relative h-full w-full bg-[#101217] text-white">
+    <div class="relative min-h-screen w-full bg-[#101217] text-white">
       <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <Header />
       <Slot />
